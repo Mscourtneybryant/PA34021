d287baf001bb197e993e59a60ce85326facb519e
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-10-10 14:06:20 -0500

    py task finish

---
+++ PythonNumberDatatype.py
@@ -0,0 +1,57 @@
+# declaring integer variables
+
+int1 = 5
+int2 = 10
+result = int1 + int2
+print(result)
+
+# declaring float variables
+
+float1 = 2.5
+float2 = 4.5
+result = float1 + float2
+print(result)
+
+# declare an integer and float
+
+int_var = 3
+float_var = 4.5
+my_result = int_var + float_var
+print(my_result)
+
+
+# divide two integers
+
+int3 = 20
+int4 = 4
+division_result = int3 / int4
+print(division_result)
+
+# divide two float variables
+
+float_var1 = 12.5
+float_var2 = 4.5
+result = float_var1 / float_var2
+print(result)
+
+# total sale
+
+coffee_price = 3.50
+cappuccino_price = 4.00
+espresso_price = 2.75
+total_sale = 3 * coffee_price + 4 * cappuccino_price + 2 * espresso_price
+print(total_sale)
+
+# total sale with tax
+
+subtotal = total_sale 
+sales_tax = 0.05
+total_sale_with_tax = subtotal + (subtotal * sales_tax)
+print(total_sale_with_tax)
+
+# area of circle
+
+radius = 7
+pi = 3.14
+area = pi * ( radius ** 2)
+print (area)
